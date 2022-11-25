3f806254ab207df4532b02e67e3e40a0868e3b41
hokkykurnia <hokky.kurnia@gmail.com>
2022-11-25 23:39:44 +1100

    prac update

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express")​
+var app = express()​
+var port = process.env.port || 3000;​
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({extends: false}))
+
+const addNumbers = (numver1, number2) => {
+    var num1 = parseInt(nuumber1)
+    var num2 = parseInt(nuumber2)
+    var result = num1+num2;
+    return result;
+}
+
+app.get("/addTwoNumbers", (req,res) => {
+    var numver1 = req.query.number1;
+    var numver2 = req.query.number2;
+    var result = addNumbers(number1, number2)
+    res.json({statusCode: 200, data: result, message:'Success'})
+})
+
+var port = process.env.port || 3000;
+
+app.listen(port,()=>{​
+    console.log("App listening to: http://localhost:"+port)​
+})
