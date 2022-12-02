a36c1e25727de65d66d97c80e68d5f77fe804948
hokkykurnia <hokky.kurnia@gmail.com>
2022-12-02 23:30:54 +1100

    prac 3

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({extends: false}))
 
-const addNumbers = (numver1, number2) => {
-    var num1 = parseInt(nuumber1)
-    var num2 = parseInt(nuumber2)
-    var result = num1+num2;
-    return result;
-}
+//const addNumbers = (numver1, number2) => {
+//    var num1 = parseInt(nuumber1)
+//    var num2 = parseInt(nuumber2)
+//   var result = num1+num2;
+//    return result;
+//}
 
-app.get("/addTwoNumbers", (req,res) => {
-    var numver1 = req.query.number1;
-    var numver2 = req.query.number2;
-    var result = addNumbers(number1, number2)
-    res.json({statusCode: 200, data: result, message:'Success'})
-})
+//app.get("/addTwoNumbers", (req,res) => {
+//    var numver1 = req.query.number1;
+//    var numver2 = req.query.number2;
+//    var result = addNumbers(number1, number2)
+//    res.json({statusCode: 200, data: result, message:'Success'})
+//})
 
 var port = process.env.port || 3000;
 
